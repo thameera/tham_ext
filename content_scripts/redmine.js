ce79c55b88547434f96a06bc5268939a5e5818a9
Thameera Senanayaka <thameera123@gmail.com>
2015-10-07 09:03:12 +0530

    Redmine support

---
+++ content_scripts/redmine.js
@@ -0,0 +1,25 @@
+$(function() {
+
+  var getFormattedTitle = function() {
+    var c = $('#content');
+    var h2 = c.find('h2');
+    if( !h2.length ) return '';
+
+    var title = h2[0].innerText;
+
+    var h3 = c.find('.subject h3');
+    if( !h3.length ) return '';
+
+    title += ': ' + h3[0].innerText;
+
+    return title;
+  };
+
+  chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    if( request.method === 'getFormattedRedmineTitle' ) {
+      sendResponse( getFormattedTitle() );
+    }
+  });
+
+});
+
---
+++ popup.js
@@ -0,0 +1,67 @@
+$(function() {
+
+  /*
+   * Redmine-related functions
+   */
+  var redmine = {
+
+    convertUrl: function( url ) {
+
+      var lk_url = 'https://222.229.226.165';
+      var jp_url =  'http://svn';
+
+      if ( url.startsWith( lk_url ) ) {
+
+        return url.replace( lk_url, jp_url );
+
+      } else if ( url.startsWith( jp_url ) ) {
+
+        return url.replace( jp_url, lk_url );
+
+      } else {
+
+        return url;
+
+      }
+
+    },
+
+    onTab: function( tab ) {
+
+      chrome.tabs.sendMessage( tab.id, {method: 'getFormattedRedmineTitle'}, function( response ) {
+        $('#redmine-bottom').val( response );
+      });
+
+      $('#redmine-top').val( tab.url );
+      $('#redmine-middle').val( redmine.convertUrl( tab.url ) );
+
+    }
+
+  }; // redmine
+
+
+  /*
+   * At the beginning, query what kind of page we're currently in
+   */
+  chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+    if( !tabs || !tabs.length )
+      return;
+
+    var tab = tabs[0];
+
+    // Redmine
+    if( tab.url.startsWith( 'https://222.229.226.165/redmine' ) )
+      redmine.onTab( tab );
+  });
+
+
+  /*
+   * Redmine copy buttons
+   */
+  $('.copy-btn').click(function(e) {
+    $(e.target).parent().parent().find('input').select();
+    document.execCommand('copy');
+  });
+
+});
+
