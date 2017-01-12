77fe0fd66565389ceb89775f8f534d87f008e0e6
Thameera Senanayaka <thameera123@gmail.com>
2017-01-12 17:23:41 +0530

    Fix buzzfeed redirect issue

--- background.js
+++ background.js
@@ -2,6 +2,9 @@
 // Source: https://github.com/jparise/chrome-utm-stripper
 chrome.tabs.onUpdated.addListener(function( tabId, changeInfo, tab ) {
   var queryStringIndex = tab.url.indexOf( '?' );
+
+  if( tab.url.indexOf('buzzfeed.com') < queryStringIndex ) return;
+
   if( tab.url.indexOf('utm_') > queryStringIndex ) {
     var stripped = tab.url.replace(
       /([\?\&]utm_(source|medium|term|campaign|content|cid|reader)=[^&#]+)/ig,
